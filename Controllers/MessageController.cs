4997a68ec6b79c836733f5e7c0490159cf9d6ca9
PelotaDeArrozDoce <ricardoliveira174567@gmail.com>
2024-07-15 16:48:55 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/MessageController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Compilador_de_Bugs.Models;
+using System.Collections.Generic;
+
+namespace Compilador_de_Bugs.Controllers
+{
+    public class MessageController : Controller
+    {
+
+        private static List<Message> messages = new List<Message>();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(messages);
+        }
+        [HttpPost]
+        public IActionResult Index(Message message)
+        {
+            messages.Add(message);
+
+            return View(messages);
+        }
+        public IActionResult Mensagem()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/Message.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Compilador_de_Bugs.Models
+{  
+        public class Message
+        {
+            public required string Content { get; set; }
+            public string Nome { get; set; } = string.Empty;
+            public string Sobrenome { get; set; } = string.Empty;
+            public required string Email { get; set; }
+            public required string Phone { get; set; }
+            
+        }
+}
